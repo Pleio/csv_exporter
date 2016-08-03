768a732e7b9e8d337ec795c4bb1f487b15df3378
bart-j <bart@jeukendrup.nl>
2016-08-03 14:13:47 +0200

    improve performance dramatically for large exports by removing ElggBatch

--- actions/download.php
+++ actions/download.php
@@ -1,5 +1,6 @@
 <?php
-
+header("Content-Type: text/csv");
+header("Content-Disposition: attachment; filename=\"export.csv\"");
 elgg_make_sticky_form("csv_exporter");
 
 $type_subtype = get_input("type_subtype");
@@ -7,8 +8,7 @@ $exportable_values = get_input("exportable_values");
 
 list($type, $subtype) = explode(":", $type_subtype);
 
-// create a temp file
-$fh = tmpfile();
+$fh = fopen("php://output", "w"); // stream to output buffer
 
 $available_values = csv_exporter_get_exportable_values($type, $subtype, true);
 $headers = array();
@@ -19,9 +19,14 @@ foreach ($exportable_values as $export_value) {
 // headers
 fwrite($fh, "\"" . implode("\";\"", $headers) . "\"" . PHP_EOL);
 
+function row_to_guid($row) {
+    return $row->guid;
+}
+
 $options = array(
 	"type" => $type,
 	"subtype" => $subtype,
+    "callback" => "row_to_guid",
 	"limit" => false
 );
 
@@ -31,10 +36,10 @@ if ($type == "user") {
 	$options["inverse_relationship"] = true;
 }
 
-$batch = new ElggBatch("elgg_get_entities_from_relationship", $options);
+$entities = elgg_get_entities_from_relationship($options);
+foreach ($entities as $guid) {
+    $entity = get_entity($guid);
 
-foreach ($batch as $entity) {
-	
 	$values = array();
 	// params for hook
 	$params = array(
@@ -42,36 +47,19 @@ foreach ($batch as $entity) {
 		"subtype" => $subtype,
 		"entity" => $entity,
 	);
-	
+
 	foreach ($exportable_values as $export_value) {
 		$params["exportable_value"] = $export_value;
-		
 		$value = elgg_trigger_plugin_hook("export_value", "csv_exporter", $params);
 		if ($value === null) {
 			$value = $entity->$export_value;
 		}
-		
 		$values[] = $value;
 	}
-	
+
 	// row
 	fwrite($fh, "\"" . implode("\";\"", $values) . "\"" . PHP_EOL);
 }
 
-// read the csv in to a var before output
-$contents = "";
-rewind($fh);
-while (!feof($fh)) {
-	$contents .= fread($fh, 2048);
-}
-
-// cleanup the temp file
-fclose($fh);
-
-// output the csv
-header("Content-Type: text/csv");
-header("Content-Disposition: attachment; filename=\"export.csv\"");
-header("Content-Length: " . strlen($contents));
-
-echo $contents;
+fclose($fp);
 exit();
